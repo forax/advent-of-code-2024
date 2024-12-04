a7e17f8b5cbc8eb1c384b2450429ebb543f90d5b
forax <forax@univ-mlv.fr>
2024-12-04 16:10:00 +0100

    add another golf version for Day 2

--- src/main/java/AdventOfCode02Golf.java
+++ src/main/java/AdventOfCode02Golf.java
@@ -1,24 +1,21 @@
 void main() {
-  var input = """
-      7 6 4 2 1
-      1 2 7 8 9
-      9 7 6 2 1
-      1 3 2 4 5
-      8 6 4 4 1
-      1 3 6 7 9
-      """;
+  var text = """
+    7 6 4 2 1
+    1 2 7 8 9
+    9 7 6 2 1
+    1 3 2 4 5
+    8 6 4 4 1
+    1 3 6 7 9
+    """;
 
   enum Order { ASCENDING, DESCENDING, INVALID }
-  println(input.lines().flatMap(l -> Arrays.stream(l.split(" ")).map(Integer::parseInt)
+  println(text.lines().filter(l -> Arrays.stream(l.split(" ")).map(Integer::parseInt)
       .gather(Gatherers.windowSliding(2))
       .map(pair -> switch (pair.getFirst() - pair.getLast()) {
-        case -3, -2, -1  -> Order.ASCENDING;
+        case -3, -2, -1 -> Order.DESCENDING;
         case 1, 2, 3 -> Order.DESCENDING;
         default -> Order.INVALID;
       })
-      .gather(Gatherer.ofSequential(() -> new Object() { Order o; }, (s,  e, _) ->  {
-        if (e != Order.INVALID && s.o == null || s.o == e) { s.o = e; return true; }
-        s.o = Order.INVALID; return false;
-      }, (s, d) -> { if (s.o != Order.INVALID) { d.push(l); }})))
-      .count());
+      .reduce((o, o2) -> o != o2 ? Order.INVALID : o)
+      .map(o -> o != Order.INVALID).orElse(true)).count());
 }
