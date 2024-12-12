487abe20adc7b30cd14ed787e1eafbb6f9e674d4
forax <forax@univ-mlv.fr>
2024-12-12 18:13:54 +0100

    use long instead of int

--- src/main/java/AdventOfCode11.java
+++ src/main/java/AdventOfCode11.java
@@ -1,23 +1,23 @@
 void main() {
-  var input = "125 17";
+  var input = "5910927 0 1 47 261223 94788 545 7771";
 
-  var map = Arrays.stream(input.split(" ")).mapToInt(Integer::parseInt).boxed()
+  var map = Arrays.stream(input.split(" ")).mapToLong(Long::parseLong).boxed()
       .collect(Collectors.groupingBy(i -> i, Collectors.counting()));
-  for(var i = 0; i < 25; i++) {
-    var newMap = new HashMap<Integer, Long>();
+  for(var i = 0; i < 75; i++) {
+    var newMap = new HashMap<Long, Long>();
     map.forEach((s, count) -> {
       if (s == 0) {
-        newMap.merge(1, count, Math::addExact);
+        newMap.merge(1L, count, Math::addExact);
         return;
       }
       var length = 1 + (int) Math.log10(s);
       if (length % 2 == 0) {
-        var divisor = (int) Math.pow(10, length / 2);
+        var divisor = (long) Math.pow(10, length / 2);
         newMap.merge(s / divisor, count, Math::addExact);
         newMap.merge(s % divisor, count, Math::addExact);
         return;
       }
-      newMap.merge(Math.multiplyExact(2024, s), count, Math::addExact);
+      newMap.merge(Math.multiplyExact(2024L, s), count, Math::addExact);
     });
     map = newMap;
   }
--- src/main/java/AdventOfCode11Golf.java
+++ src/main/java/AdventOfCode11Golf.java
@@ -4,25 +4,25 @@ import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.summingLong;
 
 void main() {
-  var input = "125 17";
+  var input = "5910927 0 1 47 261223 94788 545 7771";
 
   println(Stream.iterate(Arrays.stream(input.split(" "))
-              .mapToInt(Integer::parseInt).boxed().collect(groupingBy(i -> i, counting())),
+              .mapToLong(Long::parseLong).boxed().collect(groupingBy(i -> i, counting())),
           m -> m.entrySet().stream()
               .flatMap(e -> {
                 var s = e.getKey();
                 var count = e.getValue();
                 if (s == 0) {
-                  return Stream.of(entry(1, count));
+                  return Stream.of(entry(1L, count));
                 }
-                var length = 1 + (int) Math.log10(s);
+                var length = 1 + (long) Math.log10(s);
                 if (length % 2 == 0) {
                   var divisor = (int) Math.pow(10, length / 2);
                   return Stream.of(entry(s / divisor, count), entry(s % divisor, count));
                 }
-                return Stream.of(entry(Math.multiplyExact(2024, s), count));
+                return Stream.of(entry(Math.multiplyExact(2024L, s), count));
               })
               .collect(groupingBy(Map.Entry::getKey, summingLong(Map.Entry::getValue))))
-      .limit(26)
+      .limit(76)
       .reduce((_, v) -> v).orElseThrow().values().stream().mapToLong(v -> v).sum());
 }
