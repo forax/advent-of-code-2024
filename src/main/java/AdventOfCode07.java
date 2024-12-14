81ba1451549bd8efd29492ca36832787a916918f
forax <forax@univ-mlv.fr>
2024-12-14 20:58:52 +0100

    simplify the code

--- src/main/java/AdventOfCode07.java
+++ src/main/java/AdventOfCode07.java
@@ -1,33 +1,33 @@
-record Equation(int test, int[] values) {
-  boolean solve(List<IntBinaryOperator> ops) {
-    return solve(values[0], 1, ops);
-  }
+record Equation(int test, int[] values) { }
 
-  boolean solve(int partial, int index, List<IntBinaryOperator> ops) {
-    if (index == values.length) {
-      return partial == test;
-    }
-    var value = values[index];
-    for(var op : ops) {
-      if (solve(op.applyAsInt(partial, value), index + 1, ops)) {
-        return true;
-      }
-    }
-    return false;
-  }
+boolean solve(Equation equation, List<IntBinaryOperator> ops) {
+  return solve(equation, equation.values[0], 1, ops);
+}
 
-  static Equation parse(String line) {
-    var tokens = line.split("[: ]+");
-    var test = Integer.parseInt(tokens[0]);
-    var values = Arrays.stream(tokens, 1, tokens.length).mapToInt(Integer::parseInt).toArray();
-    return new Equation(test, values);
+boolean solve(Equation equation, int partial, int index, List<IntBinaryOperator> ops) {
+  if (index == equation.values.length) {
+    return partial == equation.test;
   }
+  var value = equation.values[index];
+  for(var op : ops) {
+    if (solve(equation, op.applyAsInt(partial, value), index + 1, ops)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+Equation parse(String line) {
+  var tokens = line.split("[: ]+");
+  var test = Integer.parseInt(tokens[0]);
+  var values = Arrays.stream(tokens, 1, tokens.length).mapToInt(Integer::parseInt).toArray();
+  return new Equation(test, values);
 }
 
 int sumOfTestValues(List<Equation> equations, List<IntBinaryOperator> ops) {
   var sum = 0;
   for(var equation : equations) {
-    if (equation.solve(ops)) {
+    if (solve(equation, ops)) {
       sum += equation.test();
     }
   }
@@ -48,6 +48,6 @@ void main() {
       """;
 
   var ops = List.<IntBinaryOperator>of(Math::addExact, Math::multiplyExact);
-  var equations = input.lines().map(Equation::parse).toList();
+  var equations = input.lines().map(this::parse).toList();
   println(sumOfTestValues(equations, ops));
 }
